24c577b5c78a7b877722be0af1b644ccc21cd3e2
jjy <jjyruby@gmail.com>
2019-12-11 13:29:58 +0800

    experiment no_std with alloc

--- rust-contract/src/lib.rs
+++ rust-contract/src/lib.rs
@@ -1,8 +1,31 @@
 #![no_std]
+#![feature(alloc_error_handler)]
+
+#[macro_use]
+extern crate alloc;
+
+mod simple_alloc;
+
+use core::cell::UnsafeCell;
+
+// Declaration of the global memory allocator
+// NOTE the user must ensure that the memory region `[0x2000_0100, 0x2000_0200]`
+// is not used by other parts of the program
+#[global_allocator]
+static HEAP: simple_alloc::BumpPointerAlloc = simple_alloc::BumpPointerAlloc {
+    head: UnsafeCell::new(0x2000_0100),
+    end: 0x2000_0200,
+};
+
+#[alloc_error_handler]
+fn oom_handler(_: core::alloc::Layout) -> ! {
+    extern "C" { fn abort() -> !; }
+    unsafe { abort() }
+}
 
 #[no_mangle]
 pub extern "C" fn contract_entry() -> isize {
-    return 13 + 29;
+    vec![13, 29].into_iter().sum()
 }
 
 #[panic_handler]
---
+++ rust-contract/src/simple_alloc.rs
@@ -0,0 +1,34 @@
+// Bump pointer allocator implementation
+
+use core::alloc::{GlobalAlloc, Layout};
+use core::ptr;
+use core::cell::UnsafeCell;
+
+// Bump pointer allocator for *single* core systems
+pub struct BumpPointerAlloc {
+    pub head: UnsafeCell<usize>,
+    pub end: usize,
+}
+
+unsafe impl Sync for BumpPointerAlloc {}
+
+unsafe impl GlobalAlloc for BumpPointerAlloc {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+            let head = self.head.get();
+
+            let align = layout.align();
+            let res = *head % align;
+            let start = if res == 0 { *head } else { *head + align - res };
+            if start + align > self.end {
+                // a null pointer signal an Out Of Memory condition
+                ptr::null_mut()
+            } else {
+                *head = start + align;
+                start as *mut u8
+            }
+    }
+
+    unsafe fn dealloc(&self, _: *mut u8, _: Layout) {
+        // this allocator never deallocates memory
+    }
+}
